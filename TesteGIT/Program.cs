68ccba45e2ecf5b05381c9d164001d4ab8c28e5a
fcobarbosa <fcobarbosa2006@hotmail.com>
2020-09-19 17:20:47 -0300

    adicionada pergunta como voce esta

--- TesteGIT/Program.cs
+++ TesteGIT/Program.cs
@@ -8,6 +8,7 @@ namespace TesteGIT
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!!");
+            Console.WriteLine("Como você está?");
             Console.ReadLine();
         }
     }
